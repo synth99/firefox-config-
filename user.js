61ef09dde9f0970a4d574cf3e1b05b3315faf6bb
Kristof Jeff Vera <businessjeff8@gmail.com>
2025-09-27 20:01:47 +0800

    Add files via upload

---
+++ user.js
@@ -0,0 +1,46 @@
+// performance tweaks
+
+user_pref("gfx.webrender.all", true);
+// GPU RENDERING 
+user_pref("layers.acceleration.force-enabled", true);
+// HARDWARE ACCELERATION
+user_pref("media.hardware-video-decoding.enabled", true);
+// VIDEO DECODE
+user_pref("network.http.max-connections", 1800);
+// MAX CONNECTIONS
+user_pref("network.http.max-persistent-connections-per-server", 10);
+// MORE CONNCECTION
+user_pref("network.dns.disablePrefetch", true);
+// REDUCE LOOKUPS
+user_pref("network.prefetch-next", false);
+// SAVES BANDWIDTH&PREVENT TRACKING
+
+//  Privacy & Security
+
+user_pref("privacy.resistFingerprinting", true);
+//ANTI FINGERPRINTING
+user_pref("privacy.trackingprotection.enabled", true);
+//BLOCK ADS&TRACKERS
+user_pref("privacy.firstparty.isolate", true);
+//COOKIES ISOLATE PER DOMAIN
+user_pref("network.cookie.cookieBehavior", 1);
+//BLOCK THIRD PARTY COOKIES
+user_pref("geo.enabled", false);
+//DISABLE GEO LOCATION
+user_pref("dom.battery.enabled", false);
+//DISABLE BATTERY INFO
+user_pref("media.peerconnection.enabled", false);
+//DISABLE WEBRTC
+
+// Personal Preferences
+
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+//WONT CLOSE LAST TAB
+user_pref("middlemouse.paste", false);
+//DISABLE MIDDLE MOUSE PASTE
+user_pref("findbar.highlightAll", true);
+//HIGHLIGHT ALL 
+user_pref("ui.key.menuAccessKeyFocuses", false);
+//PREVENT ALT IN FOCUS
+user_pref("accessibility.force_disabled", 1);
+//DISABLE ACCESSIBILITY SERVICES
